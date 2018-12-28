64e01f11b4ea23935eecd91a48314c0ba31b13d1
VoN <webdesignervon@gmail.com>
2018-12-28 14:21:17 +0100

    The final one

--- tutorial1/settings.py
+++ tutorial1/settings.py
@@ -115,18 +115,26 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
+
+
+STATIC_URL = '/static/'
+
+
+
+
+STATIC_ROOT = '/static/'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 LOGOUT_REDIRECT_URL = 'home'
 
 LOGIN_REDIRECT_URL = 'home'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 LOGIN_URL = 'login'
 
-STATIC_ROOT = 'boards/static'
-
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
