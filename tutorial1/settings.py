74707cea2882095be505d4f5173764baad590026
VoN <webdesignervon@gmail.com>
2018-12-28 12:15:22 +0100

    Added requirements.txt file

--- tutorial1/settings.py
+++ tutorial1/settings.py
@@ -1,3 +1,7 @@
+from decouple import config, Csv
+
+from dj_database_url
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -8,12 +12,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w%ja+*s(9ifkm58pf0i@8b%jrnd7nmcbxnk1f_!3qnqhiglsq#'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
@@ -70,10 +74,8 @@ WSGI_APPLICATION = 'tutorial1.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config (
+        default=config('DATABASE_URL'))
 }
 
 
