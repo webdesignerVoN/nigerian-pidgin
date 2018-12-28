9565280680549098eb135cba71c045adf6f756a0
VoN <webdesignervon@gmail.com>
2018-12-28 15:00:25 +0100

    two

--- tutorial1/settings.py
+++ tutorial1/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 
 
 
-STATIC_URL = '/static/'
+STATIC_URL = 'boards/static/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'boards/static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'boards/static'),
