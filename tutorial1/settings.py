76df3fdd4acd7210d73e74ba5e3ecd85947f33d9
VoN <webdesignervon@gmail.com>
2018-12-31 11:08:32 +0100

    Added security features

--- tutorial1/settings.py
+++ tutorial1/settings.py
@@ -1,4 +1,6 @@
+from decouple import config
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -8,12 +10,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w%ja+*s(9ifkm58pf0i@8b%jrnd7nmcbxnk1f_!3qnqhiglsq#'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv)
 
 
 # Application definition
